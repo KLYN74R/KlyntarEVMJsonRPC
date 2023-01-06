aeaad425bdcc3c26565dd2982af392f50faebbe9
Vlad Chernenko <vladchernenko@protonmail.com>
2023-01-07 00:14:09 +0200

    Add separation of useful/useless methods. Going to finish KLY-EVM

--- kly.js
+++ kly.js
@@ -22,15 +22,299 @@
 import web3 from 'web3'
 
 
+
+
+//___________________________________Used on KLY symbiotes___________________________________
+
+
+
+
 METHODS_MAPPING.set('eth_chainId',_=>CONFIG.EVM.chainId)
 
+
 METHODS_MAPPING.set('eth_protocolVersion',_=>CONFIG.EVM.protocolVersionInHex)
+
 
 //Or do smth with VERIFICATION_THREAD | GENERATION_THREAD
 METHODS_MAPPING.set('eth_syncing',_=>false)
 
 
+//Returns the current price per gas in wei
+//do it later(we should migrate to energy)
+// + we'll add more advanced way to count
+METHODS_MAPPING.set('eth_gasPrice',_=>CONFIG.EVM.gasPriceInWeiAndHex)
+
+
+// We'll return blocknumber in order to changes in KLYNTAR symbiote
+METHODS_MAPPING.set('eth_blockNumber',_=>{
+
+    //But for tests now it's 13371337
+    return '0x'+blockInHex//'0xCC07C9'
+
+})
+
+
+
+
+// We'll take balances from local storage
+METHODS_MAPPING.set('eth_getBalance',async params=>{
+
+    let [address,quantityOrTag] = params
+
+    let account = await KLY_EVM.getAccount(address)
+
+    let balanceInHexAndInWei = web3.utils.toHex(web3.utils.fromWei(account.balance.toString(),'ether'))
+
+    return balanceInHexAndInWei
+
+
+})
+
+
+
+
+// Returns the number of transactions sent from an address
+METHODS_MAPPING.set('eth_getTransactionCount',async params=>{
+
+    let [address,quantityOrTag] = params
+
+    let account = await KLY_EVM.getAccount(address)
+
+    let nonceInHex = web3.utils.toHex(account.nonce.toString())
+
+    return nonceInHex
+
+})
+
+
+
+
+// Returns the number of transactions in a block from a block matching the given block hash
+METHODS_MAPPING.set('eth_getBlockTransactionCountByHash',params=>{
+
+    let [blockHash] = params
+
+    //Again - take from storage
+
+    return '0x539'
+
+})
+
+
+
+
+// Returns the number of transactions in a block with appropriate block height
+METHODS_MAPPING.set('eth_getBlockTransactionCountByNumber',params=>{
+
+    let [blockIndex] = params
+
+    //Again - take from storage
+
+    return '0x539'
+
+})
+
+
+
+
+// Returns code at a given address
+METHODS_MAPPING.set('eth_getCode',async params=>{
+
+    let [address,quantityOrTag] = params
+
+    let account = await KLY_EVM.getAccount(address)
+
+    let codeHash = '0x'+Buffer.from(account.codeHash).toString('hex')
+
+    return codeHash
+
+})
+
+
+
+
+/*
+
+The sign method calculates an Ethereum specific signature with:
+sign(keccak256("\x19Ethereum Signed Message:\n" + len(message) + message))).
+
+*/
+METHODS_MAPPING.set('eth_sign',params=>{
+
+    let [address,message] = params
+
+    return "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
+    
+})
+
+
+
+
+// Signs a transaction that can be submitted to the network at a later time using with
+METHODS_MAPPING.set('eth_signTransaction',params=>{
+
+    let [transaction] = params
+
+    return "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
+    
+})
+
+
+
+
+// Creates new message call transaction or a contract creation, if the data field contains code
+METHODS_MAPPING.set('eth_sendTransaction',params=>{
+
+    let [transaction] = params
+
+    return "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
+    
+})
+
+
+
+
+//Creates new message call transaction or a contract creation for signed transactions
+METHODS_MAPPING.set('eth_sendRawTransaction',params=>{
+
+    // The signed transaction data
+    let [transaction] = params
+
+    // Returns 32 Bytes - the transaction hash, or the zero hash if the transaction is not yet available
+    return "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
+    
+})
+
+
+
+
+//Executes a new message call immediately without creating a transaction on the block chain
+METHODS_MAPPING.set('eth_call',params=>{
+
+    let [transaction] = params
+
+    // the return value of executed contract
+
+    // On the machine where we make .runCall({tx,block}) we return
+    // '0x'+execResult.returnValue.toString('hex') to get the exectution result(contract interaction/default tx)
+
+    return "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
+    
+})
+
+
+
+
+//Generates and returns an estimate of how much gas is necessary to allow the transaction to complete
+METHODS_MAPPING.set('eth_estimateGas',params=>{
+
+    let [transaction] = params
+
+    //getGasAmountForContractCall()
+
+    // the return value of executed contract
+    return '0x5208'
+    
+})
+
+
+
+
+METHODS_MAPPING.set('eth_getBlockByHash',params=>{
+
+    let [blockHash,fullOrNot] = params
+
+    //WE'll get block headers from storage/cache
+
+    return '0x5208'
+    
+})
+
+
+
+
+METHODS_MAPPING.set('eth_getBlockByNumber',params=>{
+
+    let [blockHash,fullOrNot] = params
+
+    //WE'll get block headers from storage/cache
+
+    return {}
+    
+})
+
+
+
+
+METHODS_MAPPING.set('eth_getTransactionByHash',params=>{
+
+    let [txHash] = params
+
+    return {}
+    
+})
+
+
+
+
+METHODS_MAPPING.set('eth_getTransactionByBlockNumberAndIndex',params=>{
+
+    let [blockNumber,txIndex] = params
+
+    return {}
+    
+})
+
+
+
+
+// Take from vm deploymentResult.receipt
+METHODS_MAPPING.set('eth_getTransactionReceipt',params=>{
+
+    let [txHash] = params
+
+    return {}
+    
+})
+
+
+
+
+//Returns an array of all logs matching a given filter object
+METHODS_MAPPING.set('eth_getLogs',params=>{
+
+    let [filterOptions] = params
+
+    //We get logs from storage
+    return ''
+    
+})
+
+
+
+
+// Returns the value from a storage position at a given address
+METHODS_MAPPING.set('eth_getStorageAt',params=>{
+
+    let [address,quantity,quantityOrTag] = params
+
+    //But for tests now it's 13371337
+
+    return '0x13371337'
+
+})
+
+
+
+
 //___________________________________Don't need in KLY symbiotes___________________________________
+
+
+
+
+
+
+
 
 METHODS_MAPPING.set('eth_coinbase',_=>CONFIG.EVM.coinbase)
 
@@ -98,275 +382,7 @@ METHODS_MAPPING.set('eth_submitHashrate',_=>
 
 
 
-
-//___________________________________Used on KLY symbiotes___________________________________
-
-
-//Returns the current price per gas in wei
-//do it later(we should migrate to energy)
-// + we'll add more advanced way to count
-
-METHODS_MAPPING.set('eth_gasPrice',_=>CONFIG.EVM.gasPriceInWeiAndHex)
-
-
-// We'll return blocknumber in order to changes in KLYNTAR symbiote
-METHODS_MAPPING.set('eth_blockNumber',_=>{
-
-    //But for tests now it's 13371337
-    return '0x'+blockInHex//'0xCC07C9'
-
-})
-
-
-
-
-// We'll take balances from local storage
-METHODS_MAPPING.set('eth_getBalance',async params=>{
-
-    let [address,quantityOrTag] = params
-
-    let account = await KLY_EVM.getAccount(address)
-
-    let balanceInHexAndInWei = web3.utils.toHex(web3.utils.fromWei(account.balance.toString(),'ether'))
-
-    return balanceInHexAndInWei
-
-
-})
-
-
-// Returns the number of transactions sent from an address
-METHODS_MAPPING.set('eth_getTransactionCount',async params=>{
-
-    let [address,quantityOrTag] = params
-
-    let account = await KLY_EVM.getAccount(address)
-
-    let nonceInHex = web3.utils.toHex(account.nonce.toString())
-
-    return nonceInHex
-
-})
-
-
-// Returns the value from a storage position at a given address
-METHODS_MAPPING.set('eth_getStorageAt',params=>{
-
-    let [address,quantity,quantityOrTag] = params
-
-    //But for tests now it's 13371337
-
-    return '0x13371337'
-
-})
-
-
-// Returns the number of transactions in a block from a block matching the given block hash
-METHODS_MAPPING.set('eth_getBlockTransactionCountByHash',params=>{
-
-    let [blockHash] = params
-
-    //Again - take from storage
-
-    return '0x539'
-
-})
-
-// Returns the number of transactions in a block with appropriate block height
-METHODS_MAPPING.set('eth_getBlockTransactionCountByNumber',params=>{
-
-    let [blockIndex] = params
-
-    //Again - take from storage
-
-    return '0x539'
-
-})
-
-
-// Returns code at a given address
-METHODS_MAPPING.set('eth_getCode',async params=>{
-
-    let [address,quantityOrTag] = params
-
-    let account = await KLY_EVM.getAccount(address)
-
-    let codeHash = '0x'+Buffer.from(account.codeHash).toString('hex')
-
-    return codeHash
-
-})
-
-/*
-
-The sign method calculates an Ethereum specific signature with:
-sign(keccak256("\x19Ethereum Signed Message:\n" + len(message) + message))).
-
-*/
-METHODS_MAPPING.set('eth_sign',params=>{
-
-    let [address,message] = params
-
-    return "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
-    
-})
-
-// Signs a transaction that can be submitted to the network at a later time using with
-METHODS_MAPPING.set('eth_signTransaction',params=>{
-
-    let [transaction] = params
-
-    return "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
-    
-})
-
-// Creates new message call transaction or a contract creation, if the data field contains code
-METHODS_MAPPING.set('eth_sendTransaction',params=>{
-
-    let [transaction] = params
-
-    return "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
-    
-})
-
-//Creates new message call transaction or a contract creation for signed transactions
-METHODS_MAPPING.set('eth_sendRawTransaction',params=>{
-
-    // The signed transaction data
-    let [transaction] = params
-
-    // Returns 32 Bytes - the transaction hash, or the zero hash if the transaction is not yet available
-    return "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
-    
-})
-
-//Executes a new message call immediately without creating a transaction on the block chain
-METHODS_MAPPING.set('eth_call',params=>{
-
-    let [transaction] = params
-
-    // the return value of executed contract
-
-    // On the machine where we make .runCall({tx,block}) we return
-    // '0x'+execResult.returnValue.toString('hex') to get the exectution result(contract interaction/default tx)
-
-    return "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b"
-    
-})
-
-
-
-//Generates and returns an estimate of how much gas is necessary to allow the transaction to complete
-METHODS_MAPPING.set('eth_estimateGas',params=>{
-
-    let [transaction] = params
-
-    //getGasAmountForContractCall()
-
-    // the return value of executed contract
-    return '0x5208'
-    
-})
-
-
-
-METHODS_MAPPING.set('eth_getBlockByHash',params=>{
-
-    let [blockHash,fullOrNot] = params
-
-    //WE'll get block headers from storage/cache
-
-    return '0x5208'
-    
-})
-
-
-
-METHODS_MAPPING.set('eth_getBlockByNumber',params=>{
-
-    let [blockHash,fullOrNot] = params
-
-    //WE'll get block headers from storage/cache
-
-    return {}
-    
-})
-
-
-
-METHODS_MAPPING.set('eth_getTransactionByHash',params=>{
-
-    let [txHash] = params
-
-    return {}
-    
-})
-
-
-METHODS_MAPPING.set('eth_getTransactionByBlockNumberAndIndex',params=>{
-
-    let [blockNumber,txIndex] = params
-
-    return {}
-    
-})
-
-// Take from vm deploymentResult.receipt
-METHODS_MAPPING.set('eth_getTransactionReceipt',params=>{
-
-    let [txHash] = params
-
-    return {}
-    
-})
-
-
-
-//___________________________ COMPILLERS ___________________________
-
-
-METHODS_MAPPING.set('eth_getCompilers',_=>{
-
-    return ['solidity']
-    
-})
-
-
-METHODS_MAPPING.set('eth_compileSolidity',params=>{
-
-    let [codeSource] = params
-
-    //Do compilation via solc
-
-    return ''
-    
-})
-
-
-METHODS_MAPPING.set('eth_compileLLL',params=>{
-
-    let [codeSource] = params
-
-    //Do compilation via solc
-
-    return ''
-    
-})
-
-
-
-METHODS_MAPPING.set('eth_compileSerpent',params=>{
-
-    let [codeSource] = params
-
-    //Do compilation via solc
-
-    return ''
-    
-})
-
-
-//___________________________ FILTERS ___________________________
+// FILTERS ___________________________
 
 //https://ethereum.org/ru/developers/docs/apis/json-rpc/#eth_newfilter
 METHODS_MAPPING.set('eth_newFilter',params=>{
@@ -423,12 +439,46 @@ METHODS_MAPPING.set('eth_getFilterLogs',params=>{
 })
 
 
-//Returns an array of all logs matching a given filter object
-METHODS_MAPPING.set('eth_getLogs',params=>{
 
-    let [filterOptions] = params
+// COMPILLERS ___________________________
 
-    //We get logs from storage
+
+METHODS_MAPPING.set('eth_getCompilers',_=>{
+
+    return ['solidity']
+    
+})
+
+
+METHODS_MAPPING.set('eth_compileSolidity',params=>{
+
+    let [codeSource] = params
+
+    //Do compilation via solc
+
+    return ''
+    
+})
+
+
+METHODS_MAPPING.set('eth_compileLLL',params=>{
+
+    let [codeSource] = params
+
+    //Do compilation via solc
+
+    return ''
+    
+})
+
+
+
+METHODS_MAPPING.set('eth_compileSerpent',params=>{
+
+    let [codeSource] = params
+
+    //Do compilation via solc
+
     return ''
     
 })
